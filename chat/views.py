09f0a4ee573e2732810ebf93592616e2abbf7fa8
Ikotun Collins <danlogan2003@gmail.com>
2023-06-13 10:04:15 +0100

    channels

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    pass
+
+def room(request, room_name):
+    return render(request, "chat/room.html", {"room_name": room_name})
